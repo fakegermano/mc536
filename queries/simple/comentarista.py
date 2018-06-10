d0cf97a33f6c8e221f013cb1d56c28ac22810435
Daniel Travieso <danielgtravieso@gmail.com>
2018-06-10 17:35:56 -0300

    testei no postgres as queries e funcionaram 2

--- queries/simple/comentarista.py
+++ queries/simple/comentarista.py
@@ -1,17 +1,17 @@
-def get_partidas(qn_id, qtipo_doc, qpais):
+def get_partidas(qnum_ID, qtipo_ID, qpais):
     """
     Funcao que retorna uma string com a query sql para as partidas que um comentarista narrou
-    :param qn_id: int
-    :param qtipo_doc: str
+    :param qnum_ID: int
+    :param qtipo_ID: str
     :param qpais: str
     :return: str
     """
     format_str = """
-      SELECT pais1,pais2,rodada
+      SELECT "pais1","pais2","rodada"
       FROM narra
-      WHERE n_id={n_id} AND tipo_doc={tipo_doc} AND pais={pais}
+      WHERE "num_ID"={num_ID} AND "tipo_ID"={tipo_ID} AND "pais"={pais}
     """
-    sql_cmd = format_str.format(n_id=qn_id, tipo_doc=qtipo_doc, pais=qpais)
+    sql_cmd = format_str.format(num_ID=qnum_ID, tipo_ID=qtipo_ID, pais=qpais)
     return sql_cmd
 
 
