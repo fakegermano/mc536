726e3962a8a89618496b87e910c329d5da7ea1b1
Daniel Travieso <danielgtravieso@gmail.com>
2018-06-10 17:54:10 -0300

    testei no postgres as queries.. algumas funcionam, outras sao desnecessarias

--- queries/simple/partida.py
+++ queries/simple/partida.py
@@ -1,15 +1,16 @@
+# FIXME(fakegermano) Query invalida, realizadano nao existe
 def get_estadio(qpaisA, qpaisB, qrodada):
     """
     Funcao que retorna string com a sql query para o estadio da partida
     :param qpaisA: str
     :param qpaisB: str
-    :param qrodada: str(?) FIXME(fakegermano): procurar tipo
+    :param qrodada: int
     :return: str
     """
     format_str = """
-      SELECT nome,cidade,capacidade 
+      SELECT "nome","cidade","capacidade" 
       FROM realizadano
-      WHERE pais1={paisA} AND pais2={paisB} AND RODADA={rodada}
+      WHERE "pais1"={paisA} AND "pais2"={paisB} AND "rodada"={rodada}
     """
     sql_cmd = format_str.format(paisA=qpaisA, paisB=qpaisB, rodada=qrodada)
     return sql_cmd
@@ -20,13 +21,13 @@ def get_arbitro(qpaisA, qpaisB, qrodada):
     Funcao que retorna string com a sql query para o(s) arbitro(s) que apitou(aram) a partida
     :param qpaisA: str
     :param qpaisB: str
-    :param qrodada: str(?) FIXME(fakegermano): procurar tipo
+    :param qrodada: int
     :return: str
     """
     format_str = """
-      SELECT n_id,tipo_doc,pais
+      SELECT "num_ID","tipo_ID","pais"
       FROM apita
-      WHERE pais1={paisA} AND pais2={paisB} AND RODADA={rodada}
+      WHERE "pais1"={paisA} AND "pais2"={paisB} AND "rodada"={rodada}
     """
     sql_cmd = format_str.format(paisA=qpaisA, paisB=qpaisB, rodada=qrodada)
     return sql_cmd
@@ -37,13 +38,13 @@ def get_comentarista(qpaisA, qpaisB, qrodada):
     Funcao que retorna string com a sql query para o(s) comentarista(s) que narrou(aram) a partida
     :param qpaisA: str
     :param qpaisB: str
-    :param qrodada: str(?) FIXME(fakegermano): procurar tipo
+    :param qrodada: int
     :return: str
     """
     format_str = """
-      SELECT n_id,tipo_doc,pais
+      SELECT "num_ID","tipo_ID","pais"
       FROM narra
-      WHERE pais1={paisA} AND pais2={paisB} AND RODADA={rodada}
+      WHERE "pais1"={paisA} AND "pais2"={paisB} AND "rodada"={rodada}
     """
     sql_cmd = format_str.format(paisA=qpaisA, paisB=qpaisB, rodada=qrodada)
     return sql_cmd
@@ -54,18 +55,19 @@ def get_torcedores(qpaisA, qpaisB, qrodada):
     Funcao que retorna string com a sql query para os torcedores que assistiram a partida
     :param qpaisA: str
     :param qpaisB: str
-    :param qrodada: str(?) FIXME(fakegermano): procurar tipo
+    :param qrodada: int
     :return: str
     """
     format_str = """
-      SELECT n_id,tipo_doc,pais
+      SELECT "num_ID","tipo_ID","pais"
       FROM assiste
-      WHERE pais1={paisA} AND pais2={paisB} AND RODADA={rodada}
+      WHERE "pais1"={paisA} AND "pais2"={paisB} AND "rodada"={rodada}
     """
     sql_cmd = format_str.format(paisA=qpaisA, paisB=qpaisB, rodada=qrodada)
     return sql_cmd
 
 
+# FIXME(fakegermano): Se tem os paises nao precisa da query (!) - Troquei jogacontra por partida pois jogacontra nao existe
 def get_paises(qpaisA, qpaisB):
     """
     Funcao que retorna string com a sql query para os paises que jogaram a partida
@@ -74,9 +76,9 @@ def get_paises(qpaisA, qpaisB):
     :return: str
     """
     format_str = """
-      SELECT pais1,pais2
-      FROM jogacontra
-      WHERE pais1={paisA} AND pais2={paisB} 
+      SELECT "pais1","pais2"
+      FROM partida
+      WHERE "pais1"={paisA} AND "pais2"={paisB} 
     """
     sql_cmd = format_str.format(paisA=qpaisA, paisB=qpaisB)
     return sql_cmd
